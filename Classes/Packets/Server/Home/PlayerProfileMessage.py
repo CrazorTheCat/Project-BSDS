4a96fa7a5fa1b419f27fde87577c3eca49213de0
8-bitHacc <koulourasba2@gmail.com>
2024-11-30 11:30:59 +0200

    Club on PlayerProfile shows

--- Classes/Packets/Server/Home/PlayerProfileMessage.py
+++ Classes/Packets/Server/Home/PlayerProfileMessage.py
@@ -1,7 +1,9 @@
-from Classes.ClientsManager import ClientsManager
+import json
+
 from Classes.Packets.PiranhaMessage import PiranhaMessage
 from Classes.Wrappers.PlayerProfile import PlayerProfile
-from Database.DatabaseHandler import DatabaseHandler
+from Database.DatabaseHandler import DatabaseHandler, ClubDatabaseHandler
+from Classes.Wrappers.AllianceHeaderEntry import AllianceHeaderEntry
 
 class PlayerProfileMessage(PiranhaMessage):
     def __init__(self, messageData):
@@ -11,9 +13,18 @@ class PlayerProfileMessage(PiranhaMessage):
     def encode(self, fields):
         db_instance = DatabaseHandler()
         playerData = db_instance.getPlayer(fields["PlayerID"])
+
         PlayerProfile.encode(self, fields, playerData)
-        self.writeBoolean(False) # TODO: Club
-        self.writeDataReference(0)
+
+        self.writeBoolean(playerData["AllianceID"] != [0, 0])
+        if playerData["AllianceID"] != [0, 0]:
+            clubdb_instance = ClubDatabaseHandler()
+            clubData = json.loads(clubdb_instance.getClubWithLowID(playerData["AllianceID"][1])[0][1])
+            memData = clubdb_instance.getMemberWithID(clubData, playerData["ID"])
+            AllianceHeaderEntry.encode(self, clubdb_instance, clubData)
+            self.writeDataReference(25, memData["Role"])
+        else:
+            self.writeDataReference(0)
 
     def decode(self):
         return {}
